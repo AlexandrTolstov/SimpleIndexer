7e9f9fdff0f49e295b72f0cd42f03bf31d537d37
AlexandrTolstov <alextav_85@mail.ru>
2019-07-12 16:40:00 +0600

    Add project files.

---
+++ SimpleIndexer/PersonCollection.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Collections;
+
+namespace SimpleIndexer
+{
+    public class PersonCollection : IEnumerable
+    {
+        private ArrayList arPeople = new ArrayList();
+        public Person this[int index]
+        {
+            get => (Person)arPeople[index];
+            set => arPeople.Insert(index, value);
+        }
+        // Cast for caller.
+        public Person GetPerson(int pos) => (Person)arPeople[pos];
+        // Insert only Person objects.
+        public void AddPerson(Person p)
+        { arPeople.Add(p); }
+        public void ClearPeople()
+        { arPeople.Clear(); }
+
+        public int Count => arPeople.Count;
+        // Foreach enumeration support.
+        IEnumerator IEnumerable.GetEnumerator() => arPeople.GetEnumerator();
+    }
+}
